b716cdf2be999dc1421c03bd515046068b74e6df
Elias-Larsson <Elias-Larsson@chasacademy.se>
2025-04-15 15:56:37 +0200

    added login

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,49 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import './style.css';
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+document.querySelector<HTMLDivElement>('nav')!.innerHTML = `
+   <img
+        src="public/lootboxlogo.svg"
+        alt="lootbox logo"
+        class="text-white text-2xl font-bold">
+      <img src="public/iconamoon_profile-fill.svg" alt="profile icon">
+`;
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const form = document.getElementById('formLogin') as HTMLFormElement;
+
+
+async function Login() {
+  const email = (document.getElementById('email') as HTMLInputElement).value;
+  const password = (document.getElementById('password') as HTMLInputElement).value;
+
+  const url = `${import.meta.env.VITE_API_URL}/api/login`;
+  // const url = `http://localhost:3003/api/login`;
+  console.log('url: ', url);
+
+  try {
+    const response = await fetch(url, {
+      method: 'POST', 
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ email, password }),
+    });
+    
+    if (!response.ok) {
+      throw new Error(`Something went wrong when fetching data: ${response.status}`);
+    }
+    
+    const data = await response.json();
+    console.log('data: ', data);
+      } catch (error) {
+    
+    console.log('Error: ', error);
+      }
+}
+
+form.addEventListener('submit', async (e) => {
+  e.preventDefault();
+
+  console.log('submit');
+  await Login();
+});
+
