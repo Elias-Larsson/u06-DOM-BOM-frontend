f981f34890c336957a8e67baf962129ebcc8ca08
Elias-Larsson <Elias-Larsson@chasacademy.se>
2025-04-20 20:39:41 +0200

    added ts files to public folder.

---
+++ public/src/layout.ts
@@ -0,0 +1,8 @@
+import './style.css';
+
+document.querySelector<HTMLDivElement>('nav')!.innerHTML = `
+   <img
+        src="lootboxlogo.svg"
+        alt="lootbox logo">
+      <img src="iconamoon_profile-fill.svg" alt="profile icon">
+`;
